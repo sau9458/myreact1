5f5527d2ad5e0b92d56321247096a3631d4d716f
saurabh singh <sau9458@gmail.com>
2018-02-05 16:26:38 +0530

    changes applied

---
+++ src/AddOption.js
@@ -0,0 +1,73 @@
+
+import React from 'react';
+import Todo from './index.js'
+let btnStyle = {
+
+	}
+ class AddOption extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAddOption = this.handleAddOption.bind(this);
+    this.handleAddOptions = this.handleAddOptions.bind(this);
+    this.state = {
+      error: undefined,
+      options:[]
+    };
+  }
+  handleAddOption(e) {
+
+    e.preventDefault(e);
+    const option = e.target.elements.option.value.trim();
+    const error = this.handleAddOptions(option);
+    e.target.elements.option.value="";
+
+    this.setState(() => {
+      return { error };
+    });
+  }
+
+handleAddOptions(option){
+    if (!option) {
+      btnStyle = {
+      			border: '1px solid red'
+      		}
+  return false;
+    }
+
+else{
+    this.setState((prevState) => {
+      return {
+        options: prevState.options.concat(option)
+      };
+    });
+    btnStyle = {
+          border: '1px solid grey'
+        }
+  }
+}
+  //handleAddOption(e) {
+    // e.preventDefault();
+    // const option = e.target.elements.option.value.trim();
+    // const error = this.handleAddOption(option);
+    // e.target.elements.option.value="";
+    //
+    // this.setState(() => {
+    //   return { error };
+    // });
+  //}
+  render() {
+    return (
+      <div>
+        {
+          this.state.options.map((option) => <li key={option} className="list-group-item" ><input type="checkbox"/>{option}<span className="badge">checked</span></li>)
+        }
+        {this.state.error && <li className="list-group-item">{this.state.error}</li>}
+        <form onSubmit={this.handleAddOption}><br/>
+          <input type="text" name="option" className="form-control"style={btnStyle} />
+          <button className="btn btn-default">Add Option</button>
+        </form>
+      </div>
+    );
+  }
+}
+export default AddOption;
--- src/index.js
+++ src/index.js
@@ -2,40 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import input from './input.json';
+import AddOption from './AddOption.js'
 import registerServiceWorker from './registerServiceWorker';
 let btnStyle = {
-		border: '1px solid grey'
+		border: '1px solid red'
 	}
-class Todo extends React.Component{
+export class Todo extends React.Component{
   constructor(){
     super();
-    this.handleAddOption = this.handleAddOption.bind(this);
+    // this.handleAddOption = this.handleAddOption.bind(this);
     this.state={
       data:input.data,
-      options:[]
+      // options:[]
     }
   }
-  handleAddOption(option) {
-    if (!option) {
-      btnStyle = {
-      			border: '1px solid red'
-      		}
-  return false;
-    }
-
-else{
-    this.setState((prevState) => {
-      return {
-        options: prevState.options.concat(option)
-
-      };
-    });
-    btnStyle = {
-          border: '1px solid grey'
-        }
-  }
-
-  }
+//   handleAddOption(option) {
+//     if (!option) {
+//
+//       btnStyle = {
+//       			"background-color": ' red'
+//       		}
+//   return false;
+//     }
+//
+// else{
+//     this.setState((prevState) => {
+//       return {
+//         options: prevState.options.concat(option)
+//
+//       };
+//     });
+//     btnStyle = {
+//           border: '1px solid grey'
+//         }
+//   }
+//
+//   }
 
 render(){
   const jsondata=this.state.data.map((data,id)=><li className="list-group-item" key={data.id}><input type="checkbox"/>
@@ -46,7 +48,7 @@ render(){
         <ul>
             <li><h2>ToDo</h2></li>
              {jsondata}
-            <AddOption handleAddOption={this.handleAddOption} options={this.state.options}/>
+            {<li><AddOption /*handleAddOption={this.handleAddOption} options={this.state.options}*//></li> }
 
           </ul>
       </div>
@@ -54,42 +56,7 @@ render(){
  }
 
 }
-
-
-class AddOption extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.state = {
-      error: undefined
-    };
-  }
-  handleAddOption(e) {
-    e.preventDefault();
-
-    const option = e.target.elements.option.value.trim();
-    const error = this.props.handleAddOption(option);
-    e.target.elements.option.value="";
-
-    this.setState(() => {
-      return { error };
-    });
-  }
-  render() {
-    return (
-      <div>
-        {
-          this.props.options.map((option) => <li key={option} className="form-control" ><input type="checkbox"/>{option}</li>)
-        }
-        {this.state.error && <li className="list-group-item">{this.state.error}</li>}
-        <form onSubmit={this.handleAddOption}><br/>
-          <input type="text" name="option" className="form-control"style={btnStyle} />
-          <button className="btn btn-default">Add Option</button>
-        </form>
-      </div>
-    );
-  }
-}
 ReactDOM.render(<Todo />,document.getElementById('root'));
 
 registerServiceWorker();
+export default Todo;
