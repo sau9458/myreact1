d7ac1fac6aa8517228635fdcd3894f22472924da
saurabh singh <sau9458@gmail.com>
2018-02-07 18:05:45 +0530

    final code of todo

--- src/AddOption.js
+++ src/AddOption.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-
+let i=0;
 let btnStyle = {
 
 	}
@@ -9,10 +9,13 @@ let btnStyle = {
     super(props);
     this.handleAddOption = this.handleAddOption.bind(this);
     this.handleAddOptions = this.handleAddOptions.bind(this);
+		this.hidebadge=this.hidebadge.bind(this);
     this.state = {
       error: undefined,
-      options:[]
+      options:[],
+			visibility:false
     };
+		
   }
   handleAddOption(e) {
 
@@ -38,28 +41,33 @@ else{
     this.setState((prevState) => {
       return {
         options: prevState.options.concat(option)
+
       };
     });
     btnStyle = {
           border: '1px solid grey'
         }
   }
+
 }
-  //handleAddOption(e) {
-    // e.preventDefault();
-    // const option = e.target.elements.option.value.trim();
-    // const error = this.handleAddOption(option);
-    // e.target.elements.option.value="";
-    //
-    // this.setState(() => {
-    //   return { error };
-    // });
-  //}
+
+hidebadge(){
+	this.setState((prevState,e)=>{
+		return{
+visibility:!prevState.visibility
+
+};
+
+	});
+}
+
   render() {
     return (
       <div>
         {
-          this.state.options.map((option) => <li key={option} className="list-group-item" ><input type="checkbox"/>{option}<span className="badge">checked</span></li>)
+          this.state.options.map((option) => <li key={option} className="list-group-item" >
+						<input type="checkbox" onClick={this.hidebadge} id={i++} key={option}/>
+						{option}{this.state.visibility &&(<span className="badge" name="checkbox">checked </span>)}</li>)
         }
         {this.state.error && <li className="list-group-item">{this.state.error}</li>}
         <form onSubmit={this.handleAddOption}><br/>
